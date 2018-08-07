9772529be5452c469c9acb64346530136bd6d74c
Yiwen Yang <381725454@qq.com>
2018-08-06 22:05:28 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+<<<<<<< HEAD
+// Define color variables:
+
+letterColors = [red, orange, green, blue, purple];
+
+// This variable controls the smallest distance at which a mouse will 
+mouseResponseThreshold = 20;
+
+// This variable controls how strongly the dots will try to return to their starting position
+friction = 0.85;
+
+// This variable controls how much the dots will rotate when interacting
+rotationForce = 0.01;
+
+message = 'Yiwen Yang';
+
+drawName(message, letterColors);
+bounceBubbles();
+=======
+// Define color variables:
+
+letterColors = [red, orange, green, blue, purple];
+
+// This variable controls the smallest distance at which a mouse will 
+mouseResponseThreshold = 20;
+
+// This variable controls how strongly the dots will try to return to their starting position
+friction = 0.85;
+
+// This variable controls how much the dots will rotate when interacting
+rotationForce = 0.01;
+
+message = 'Yiwen Yang';
+
+drawName(message, letterColors);
+bounceBubbles();
+>>>>>>> Aristotleyyw-patch-1
